18d13b56e044226778d5d06da574709fce14dc1f
LOGAN-bruh <9721555@graniteschools.org>
2026-02-10 12:00:57 -0700

    Add functions to generate themed stories

--- src/story.py
+++ src/story.py
@@ -1,4 +1,14 @@
-story1 = (f"Today at lunch, I ordered the {adjectives} {nouns} with a side of {adjectives} {nouns}. When I tried to {verbs} it, the {nouns} started to {verbs} across the table! My {adjectives} friend tried to {verbs} it with a {nouns}, but that only made things {adjectives}. Now there's {nouns} all over my {adjectives} {nouns} and the lunch lady wants me to {verbs} the entire {adjectives} {nouns}.")
-story2 = (f"I just created the most {adjectives} {nouns} ever! All you have to do is {verbs} the {adjectives} {nouns} and it will automatically {verbs} your {nouns}. My {adjectives} teacher said I could {verbs} it at the {adjectives} science fair. I'm going to {verbs} all day to make sure the {nouns} works perfectly!")
-story3 = (f"Our family decided to {verbs} to a {adjectives} {nouns} for vacation. When we got there, the {nouns} was completely {adjectives}! Dad tried to {verbs} the situation by finding a {adjectives} {nouns}, but Mom wanted to just {verbs} home. My {adjectives} sister wouldn't stop trying to {verbs} every {nouns} she saw!")
-story4 = (f"For the {adjectives} talent show, I decided to {verbs} while balancing a {nouns} on my {adjectives} {nouns}. Everyone thought I was {adjectives}, but I practiced how to {verbs} for weeks! When it was my turn, I walked onto the {adjectives} stage and began to {verbs}. The {adjectives} audience couldn't believe their {nouns}!")
+def get_story1(adj, nn, vb):
+    return f"Today at lunch, I ordered the {adj[0]} {nn[0]} with a side of {adj[1]} {nn[1]}. When I tried to {vb[0]} it, the {nn[2]} started to {vb[1]} across the table! My {adj[2]} friend tried to {vb[2]} it with a {nn[3]}, but that only made things {adj[3]}. Now there's {nn[4]} all over my {adj[4]} {nn[5]} and the lunch lady wants me to {vb[3]} the entire {adj[5]} {nn[6]}."
+
+def get_story2(adj, nn, vb):
+    # Theme: Space Adventure (5 adj, 5 nn, 5 vb)
+    return f"In a {adj[0]} galaxy, a brave {nn[0]} decided to {vb[0]} through a black hole. They carried a {adj[1]} {nn[1]} designed to {vb[1]} time itself. Suddenly, a {adj[2]} alien appeared and tried to {vb[2]} the ship's {nn[2]}. 'Don't {vb[3]} my {adj[3]} {nn[3]}!' the captain yelled. With one {adj[4]} {nn[4]}, they managed to {vb[4]} safely back to Earth."
+
+def get_story3(adj, nn, vb):
+    # Theme: Haunted House (5 adj, 5 nn, 5 vb)
+    return f"I stepped into the {adj[0]} mansion and felt a {nn[0]} start to {vb[0]} behind me. The air smelled like {adj[1]} {nn[1]}, making it hard to {vb[1]}. I saw a {adj[2]} ghost try to {vb[2]} a heavy {nn[2]} near the stairs. I grabbed my {adj[3]} {nn[3]} and began to {vb[3]} as fast as I could! Luckily, I found a {adj[4]} {nn[4]} that helped me {vb[4]} out the front door."
+
+def get_story4(adj, nn, vb):
+    # Theme: Cooking Disaster (5 adj, 5 nn, 5 vb)
+    return f"To win the {adj[0]} bake-off, I had to {vb[0]} a giant {nn[0]}. I added a {adj[1]} {nn[1]} to the mix, but it started to {vb[1]} uncontrollably! My {adj[2]} coach told me to {vb[2]} the {nn[2]} before it exploded. 'Use the {adj[3]} {nn[3]}!' they shouted. I tried to {vb[3]} the mess, but ended up covered in {adj[4]} {nn[4]}. I guess I'll {vb[4]} better next time."
